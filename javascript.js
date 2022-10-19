35976959024922429a736d289fc1b5d8f7f246d0
AlexisFaundez <alfaundez.dwfsc3@bootcampudd.cl>
2022-10-18 21:22:12 -0300

    Agregar detalles e inicio de js

--- javascript.js
+++ javascript.js
@@ -1,51 +1,25 @@
-mostrarItem();
+const escribirObjeto = document.getElementById("escribirObjeto")
+const listaObjetos = document.getElementById("listaObjetos")
 
-let addItemInput = document.getElementById("writeItem")
-let addItemBtn = document.getElementById("addItemBtn")
+let arrayObjetos = [];
 
-addItemBtn.addEventListener("click", function(){
 
-const writeItemValue = writeItemValue.value;
 
-if(writeItemValue){
-    let webUser = localStorage.getItem("localItem");
 
-    if (webUser == null){
-        userObject = [];
+const CrearItem = (objeto) => {
+    let item = {
+        objeto: objeto,
+        estado: false
     }
-    else{
-        userObject = JSON.parse(webUser);
-    }
-    userObject.push({
-            
-        'item':addUserInputValue
-    });
-    localStorage.setItem("localItem", JSON.stringify(userObject));
-        addUserInput.value = '';
-    }
-mostrarItem();
-})
 
-function mostrarItem(){
-    let webUser = localStorage.getItem("localItem");
-    if(webUser == null){
-        userObject = [];
-    }
-    else{
-        userObject = JSON.parse(webUser);
-    }
-    let html = '';
-    let addedUserList = document.getElementById("addedItemList");
-    userObject.forEach((item, index) => {
-        html += `<tr>
-        <th scope="row">${index+1}</th>
+arrayObjetos.push(item);
 
-        <td>${item.user}</td>
-
-        <td><button type="button" onclick="editUser(${index})" class="text-primary"><i class="fa fa-edit"></i>Editar</button></td>
-        
-        <td><button type="button" onclick="deleteUser(${index})" class="text-danger"><i class="fa fa-trash"></i>Eliminar</button></td>
-    </tr>`;
-    addedUserList.innerHTML = html;
-});
+return(item)
 }
+
+let comida = CrearItem("Manzana");
+let fruta = CrearItem("Pera")
+
+console.log(comida)
+
+console.log(arrayObjetos)
