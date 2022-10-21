d1878f2526e761c173ddf23d61ac856897690bd8
AlexisFaundez <alfaundez.dwfsc3@bootcampudd.cl>
2022-10-21 17:12:01 -0300

    Cambios boton submit

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
-const escribirObjeto = document.getElementById("escribirObjeto")
+const formLista = document.getElementById("formLista")
 const listaObjetos = document.getElementById("listaObjetos")
 
 let arrayObjetos = [];
-
-
 
 
 const CrearItem = (objeto) => {
@@ -12,14 +10,48 @@ const CrearItem = (objeto) => {
         estado: false
     }
 
-arrayObjetos.push(item);
+    arrayObjetos.push(item);
 
-return(item)
+    return item;
 }
 
-let comida = CrearItem("Manzana");
-let fruta = CrearItem("Pera")
+const GuardarDB = (objeto) => {
 
-console.log(comida)
+    localStorage.setItem("Lista", JSON.stringify(arrayObjetos))
+}
 
-console.log(arrayObjetos)
+const CrearLista = () => {
+    listaObjetos.innerHTML = "";
+    arrayObjetos = JSON.parse(localStorage.getItem("Lista"));
+    
+    if(arrayObjetos === null){
+        arrayObjetos = [];
+    }else{
+        arrayObjetos.forEach(element => {
+        listaObjetos.innerHTML += `<div class="alert alert-dark" role="alert">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+          <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+        </svg>
+        <b> Artículo de compras </b> - Status
+        <span class="float-end">
+          <i class="bi bi-gear"></i>
+          <i class="bi bi-trash"></i>
+        </span>
+      </div>`
+        });
+    }
+}
+
+formLista.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let formLista = document.getElementById("escribirObjeto").value;
+
+    console.log(formLista)
+
+    CrearItem(formLista);
+    GuardarDB();
+
+});
+
+
+document.addEventListener("DOMContentLoaded", CrearLista)
