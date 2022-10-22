386275df3d94a2807171b12150da5a9b38f112ac
AlexisFaundez <alfaundez.dwfsc3@bootcampudd.cl>
2022-10-22 20:08:58 -0300

    Working, only needs some last details

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-
 let escribirObjeto = document.getElementById("escribirObjeto");
 let addItemBtn = document.getElementById("addItemBtn");
 
@@ -8,7 +7,7 @@ addItemBtn.addEventListener("click", (e) => {
     const escribirObjetoValue = escribirObjeto.value;
 
     if (escribirObjetoValue) {
-        let localObject = localStorage.getItem("localUser")
+        let localObject = localStorage.getItem("savedItem")
         if (localObject == null) {
             objeto = [];
         }
@@ -19,39 +18,73 @@ addItemBtn.addEventListener("click", (e) => {
         objeto.push({
             "objeto": escribirObjetoValue
         });
-        localStorage.setItem("localUser", JSON.stringify(objeto))
+        localStorage.setItem("savedItem", JSON.stringify(objeto))
         escribirObjeto.value = '';
     }
-mostrarObjeto();
+    mostrarObjeto();
 })
 
 function mostrarObjeto() {
-    let localObject = localStorage.getItem("localUser")
-    if (localObject == null){
+    let localObject = localStorage.getItem("savedItem")
+    if (localObject == null) {
         objeto = [];
     } else {
-        localObject = JSON.parse(localObject)
+        objeto = JSON.parse(localObject)
     }
-    let html = "";
+    let objetoagregado = "";
     let listaObjetos = document.getElementById("listaObjetos");
     objeto.forEach((item, index) => {
-        html += `<tr>
-        <th scope="row">${index+1}</th>
+        objetoagregado += `<tr>
+        <th scope="row">${index + 1}</th>
 
         <td><b>${item.objeto}</b></td>
-
-        <td><button type="button" onclick="editUser(${index})" class="btn btn-primary float-end"><i class="fa fa-edit"></i>Editar</button></td>
+        <td><input class="form-check-input" type="checkbox"></td>
+        <td><button type="button" onclick="editarObjeto(${index})" class="btn btn-primary float-end"><i class="fa fa-edit"></i>Editar</button></td>
         
         <td><button type="button" onclick="borrarObjeto(${index})" class="btn btn-danger float-end"><i class="fa fa-trash"></i>Eliminar</button></td>
     </tr>`;
     });
-    listaObjetos.innerHTML = html;
+    listaObjetos.innerHTML = objetoagregado;
+}
+// borrarObjeto
+function borrarObjeto(index) {
+    let localObject = localStorage.getItem("savedItem")
+    let objeto = JSON.parse(localObject)
+    objeto.splice(index, 1);
+    localStorage.setItem("savedItem", JSON.stringify(objeto));
+    mostrarObjeto();
 }
 
-function borrarObjeto(index){
-    let localObject = localStorage.getItem("localUser")
-    let userObject = JSON.parse(localObject)
-    userObject.splice(index, 1);
-    localStorage.setItem("localUser", JSON.stringify(userObject));
-    mostrarObjeto()
+
+// editarObjeto
+function editarObjeto(index) {
+    let editarTexto = document.getElementById("objetoGuardado");
+    let addItemBtn = document.getElementById("addItemBtn");
+    let editItemBtn = document.getElementById("editItemBtn");
+    let localObject = localStorage.getItem("savedItem");
+    let objeto = JSON.parse(localObject);
+    editarTexto.value = index;
+    escribirObjeto.value = objeto[index]['objeto'];
+    addItemBtn.style.display = "none";
+    editItemBtn.style.display= "inline-block"
 }
+// editItemBtn
+
+let editItemBtn = document.getElementById("editItemBtn");
+editItemBtn.addEventListener("click", function (e) {
+    let addItemBtn = document.getElementById("addItemBtn");
+    let localObject = localStorage.getItem("savedItem")
+    let objeto = JSON.parse(localObject);
+    let objetoGuardado = document.getElementById("objetoGuardado").value;
+
+    for (keys in objeto[objetoGuardado]) {
+        if (keys == 'objeto') {
+            objeto[objetoGuardado].objeto = escribirObjeto.value
+        }
+    }
+    editItemBtn.style.display = "none"
+    addItemBtn.style.display = "inline-block"
+    localStorage.setItem("savedItem", JSON.stringify(objeto))
+    escribirObjeto.value = '';
+    mostrarObjeto();
+})
